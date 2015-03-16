0e15746ede23fcb38063040d3f902a6b47392fe0
Ted Crossman <tedo@signalfuse.com>
2015-03-16 13:04:18 -0700

    Add patched version of makeself

--- create_installer.sh
+++ create_installer.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-makeself --tar-extra '-X makeself.excludes' `pwd` signalfx-configure-collectd.sh "SignalFx CollectD configuration tool" ./install.sh
+#!/bin/bash
+SCRIPTDIR=$(cd $(dirname $0) 2>/dev/null && pwd)
+
+cd ${SCRIPTDIR}
+./makeself --tar-extra '-X makeself.excludes' `pwd` signalfx-configure-collectd.sh "SignalFx CollectD configuration tool" ./install.sh
