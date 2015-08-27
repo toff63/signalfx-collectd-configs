b5cd90694deb7abce435e2bdcd5802e855d0d218
Straten Schemel <straten@signalfx.com>
2015-08-27 11:14:19 -0700

    Prepare collectd install for use via curl

---
+++ collectd-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dlfile=$(mktemp -t sfx-download-XXXXXX)
+
+clean_up() {
+ rm $dlfile;
+ trap 0 
+ exit;
+}
+
+trap clean_up 0 1 2 3 15
+curl -sSL $(curl -s https://api.github.com/repos/signalfx/signalfx-collectd-configs/releases | grep browser_download_url | grep "collectd-package-install" | head -n 1 | cut -d '"' -f 4) > $dlfile
+
+/bin/bash $dlfile "$@"
