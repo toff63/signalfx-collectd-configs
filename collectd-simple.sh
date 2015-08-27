32516b32e6f39c7cb673756e91dbed391fcc7d7c
Ted Crossman <tedo@signalfuse.com>
2015-08-27 11:10:21 -0700

    Fix collectd-simple.sh to only look for signalfx-configure-collectd.sh

--- collectd-simple.sh
+++ collectd-simple.sh
@@ -9,7 +9,7 @@ clean_up() {
 }
 
 trap clean_up 0 1 2 3 15
-curl -sSL $(curl -s https://api.github.com/repos/signalfx/signalfx-collectd-configs/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) > $dlfile
+curl -sSL $(curl -s https://api.github.com/repos/signalfx/signalfx-collectd-configs/releases | grep browser_download_url | grep "signalfx-configure-collectd.sh" | head -n 1 | cut -d '"' -f 4) > $dlfile
 
 /bin/bash $dlfile -- "$@"
 
