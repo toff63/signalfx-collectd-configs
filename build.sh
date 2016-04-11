ce5518d013f67b26287f0d0010ca31ef4cdfe9a4
Matthew Pound <mwp@signalfx.com>
2016-04-11 14:27:14 -0700

    delete everything accidentally

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-rm -rf managed_config *.conf install.sh
+rm -rf managed_config collectd.conf.tmpl
 mkdir -p managed_config
 
 for x in collectd-signalfx/10-signalfx.conf collectd-write_http/10-write_http-plugin.conf collectd-aggregation/10-aggregation-cpu.conf; do
@@ -10,6 +10,7 @@ for x in collectd-signalfx/10-signalfx.conf collectd-write_http/10-write_http-pl
 done
 curl -sSL "https://raw.githubusercontent.com/signalfx/integrations/master/collectd/collectd.conf.tmpl" > collectd.conf.tmpl
 
-./create_installer.sh
+tar -cvzf install-files.tgz managed_config filtering_config collectd.conf.tmpl get_aws_unique_id
 
 aws s3 cp install.sh s3://public-downloads--signalfuse-com/collectd-install-test --cache-control="max-age=0, no-cache"
+aws s3 cp install-files.tgz s3://public-downloads--signalfuse-com/install-files-test.tgz --cache-control="max-age=0, no-cache"
--- create_installer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPTDIR=$(cd $(dirname $0) 2>/dev/null && pwd)
-
-cd ${SCRIPTDIR}
-./makeself --nox11 --tar-extra '-X makeself.excludes' `pwd` install.sh "SignalFx CollectD configuration tool" ./run.sh
